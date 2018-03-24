7af9ea54ec3eddeada71b4fba4c262a3391596ee
Haley Mendoza <mendozahaley@gmail.com>
2018-03-23 20:41:41 -0700

    updated README. Fixed nav bar handle.

--- lina-haley/scripts/articleView.js
+++ lina-haley/scripts/articleView.js
@@ -72,7 +72,7 @@ articleView.handleMainNav = function() {
   // TODONE: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-  $('main.nav').on('click', '.tab', function (event){
+  $('.main-nav').on('click', '.tab', function (event){
     event.preventDefault();
     $('.tab-content').hide();
     $(`#${$(this).data('content')}`).fadeIn();
